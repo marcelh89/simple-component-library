ba5d7d25bdcde56a4f7d6ebf61e5e180db017165
Marcel Hinderlich <m.hinderlich@reply.de>
2019-01-31 09:37:17 +0100

    added missing libs and dist folder

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TextInput = undefined;
+
+var _TextInput = require("./components/TextInput");
+
+var _TextInput2 = _interopRequireDefault(_TextInput);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.TextInput = _TextInput2.default;
